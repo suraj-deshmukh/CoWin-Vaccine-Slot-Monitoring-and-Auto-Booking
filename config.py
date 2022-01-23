9935a816ddf4fa6abde5cc5c70f604cffe2ebc0d
Suraj Deshmukh <suraj-deshmukh@users.noreply.github.com>
2022-01-24 02:51:42 +0530

    Update config.py
    added support for 15 age group and precaution dose

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 vaccine = [("COVAXIN","Covaxin"), ( "COVISHIELD", "Covishield"), (( "ANY", "Any"))]
 vaccine_multi = ["COVAXIN","COVISHIELD", "SPUTNIK V"]
 fee = [("Free", "Free"), ("Paid", "Paid"), ("Any", "Any")]
-dose = [("1", "1"),("2", "2")]
-age = [("18+", "18+"),("18", "18-44"),("45", "45+")]
+dose = [("1", "1"),("2", "2"),("precaution dose", "precaution dose")]
+# age = [("18+", "18+"),("18", "18-44"),("45", "45+")]
+age = [("18", "18+"),("15", "15-18")]
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0',
     'Accept': 'application/json, text/plain, */*',
